e860e4e01fec039d8c44ecbc611e9fc4123e35e5
Wonser <ilyailyailya.ilya@yandex.ru>
2025-10-27 04:09:31 +0300

    vanila new page

--- src/js/vanila.js
+++ src/js/vanila.js
@@ -1,28 +1,33 @@
 $(document).ready( function() {
   let galleryThumbs = new Swiper('.roadmap__images', {
     spaceBetween: 12,
-    slidesPerView: 'auto',
+    slidesPerView: 1,
     loop: true,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
     centeredSlides: true,
-    speed: 3000,
-    autoplay: {
-      delay: 0,
-      disableOnInteraction: false,
-    },
     pagination: {
       el: ".roadmap .swiper-pagination",
       // clickable: true,
       dynamicBullets: true, 
       dynamicMainBullets: 1,
     },
+    autoplay: {
+      enabled: false,
+    },
     breakpoints: {
       991: {
+        slidesPerView: 'auto',
         spaceBetween: 10,
         allowTouchMove: false,
         direction: 'vertical',
         freeMode: true,
+        speed: 3000,
+        autoplay: {
+          enabled: true,
+          delay: 0,
+          disableOnInteraction: false,
+        },
       },
     }
   });
@@ -39,14 +44,20 @@ $(document).ready( function() {
     thumbs: {
       swiper: galleryThumbs,
     },
-    speed: 400,
+    
     autoplay: {
-      delay: 2750,
-      disableOnInteraction: false,
+      enabled: false,
     },
     breakpoints: {
       991: {
         allowTouchMove: false,
+        autoplay: true,
+        speed: 400,
+        autoplay: {
+          enabled: true,
+          delay: 2750,
+          disableOnInteraction: false,
+        },
       },
     }
   });
@@ -54,5 +65,89 @@ $(document).ready( function() {
 
   $('.version-card').on('click', function(){
     $(this).toggleClass('active').siblings().removeClass('active');
+  });
+
+  $('.way__item-name').hover(function(){
+    $(this).parent().addClass('current').siblings().addClass('hide');
+  }, function(){
+    $(this).parent().removeClass('current').siblings().removeClass('hide');
+  });
+
+
+  var audio = $('#myAudio')[0]; // Get the native HTML audio element
+  var playPauseBtn = $('#play-pause-btn');
+  var progressBar = $('#progress-bar');
+  var currentTimeSpan = $('#current-time');
+  var durationSpan = $('#duration');
+  var volumeBar = $('#volume-bar');
+
+  // Play/Pause functionality
+  playPauseBtn.on('click', function() {
+    if (audio.paused) {
+        audio.play();
+        playPauseBtn.toggleClass('pause');
+    } else {
+        audio.pause();
+        playPauseBtn.toggleClass('pause');
+    }
+  });
+
+  // Update progress bar and time display
+  $(audio).on('timeupdate', function() {
+      var percentage = (audio.currentTime / audio.duration) * 100;
+      progressBar.val(percentage);
+      progressBar.css('background-size', progressBar.val() + '% 100%');
+      var currentMinutes = Math.floor(audio.currentTime / 60);
+      var currentSeconds = Math.floor(audio.currentTime - currentMinutes * 60);
+      currentTimeSpan.text((currentMinutes < 10 ? '0' : '') + currentMinutes + ':' + (currentSeconds < 10 ? '0' : '') + currentSeconds);
+  });
+
+  // Set duration when metadata is loaded
+  $(audio).on('loadedmetadata', function() {
+      var durationMinutes = Math.floor(audio.duration / 60);
+      var durationSeconds = Math.floor(audio.duration - durationMinutes * 60);
+      durationSpan.text((durationMinutes < 10 ? '0' : '') + durationMinutes + ':' + (durationSeconds < 10 ? '0' : '') + durationSeconds);
+  });
+
+  // Seek functionality
+  progressBar.on('change', function() {
+      var time = audio.duration * (progressBar.val() / 100);
+      audio.currentTime = time;
+  });
+
+  progressBar.on('input', function() {
+      progressBar.css('background-size', progressBar.val() + '% 100%');
+  });
+
+  // Volume control
+  volumeBar.on('input', function() {
+      audio.volume = volumeBar.val() / 100;
+      volumeBar.css('background-size', volumeBar.val() + '% 100%');
+
+      if(volumeBar.val() == 0) {
+        $('.volume-icon').addClass('hide');
+      } else {
+        $('.volume-icon').removeClass('hide');
+      }
+  });
+
+  $('.volume-icon').on('click', function(){
+    if($(this).hasClass('hide')) {
+      $(this).removeClass('hide');
+      volumeBar.val(50).trigger('input');
+    } else {
+      $(this).addClass('hide');
+      volumeBar.val(0).trigger('input');
+    }
+  });
+
+  // Handle audio ending
+  $(audio).on('ended', function() {
+      progressBar.val(0);
+      currentTimeSpan.text('0:00');
+  });
+
+  $('.faq-vanila__timecode').on('click', function(){
+    audio.currentTime = $(this).data('time');
   });
 }); 
